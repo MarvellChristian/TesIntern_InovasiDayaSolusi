08a2609a1e5b81e6dd075dfbfd2289bd65c42ea2
marvell <marvellchristian1613@gmail.com>
2023-10-19 15:55:08 +0700

    update logic

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -4,12 +4,18 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Transaction;
+use App\Models\Status;
 
 class TransactionController extends Controller
 {
     public function view()
     {
         $transactions = Transaction::with('status')->get();
-        return response()->json($transactions);
+        $status = Status::all();
+        $data = [
+            'transactions' => $transactions,
+            'status' => $status,
+        ];
+        return response()->json($data);
     }
 }
